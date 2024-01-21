085c1dc2999254409b46c199ffed8eb7262a033c
Andres Jose Borek <andres.b.dev@gmail.com>
2024-01-20 16:34:25 -0800

    Replace crypto with unixcrypt for password encryption
    The password encryption used in the application has been switched from the crypto library to the unixcrypt library. This change simplifies the password encryption function by eliminating unnecessary steps such as generating a random salt and creating a hash. It also includes updates to both the package.json and package-lock.json files to include the unixcrypt dependency.

--- app/utils/password.ts
+++ app/utils/password.ts
@@ -1,5 +1,4 @@
-import crypto from "crypto";
-
+import unixcrypt from "unixcrypt";
 /**
  * Crypts the given password using SHA-512 algorithm and returns the encrypted password in crypt library format.
  *
@@ -7,14 +6,5 @@ import crypto from "crypto";
  * @returns {string} - The encrypted password in crypt library format.
  */
 export function cryptPassword(password: string): string {
-  // The id for SHA-512 in the `crypt` library is 6
-  const id = "6";
-  // Generate a random salt
-  const salt = crypto.randomBytes(8).toString("hex");
-
-  const hash = crypto.createHash("sha512");
-  hash.update(password + salt);
-
-  // The format of the `crypt` library password is $id$salt$encrypted
-  return "$" + id + "$" + salt + "$" + hash.digest('hex');
+  return unixcrypt.encrypt(password)
 }
