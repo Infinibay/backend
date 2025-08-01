df1bb474c16e57a90626ebc6eed3c9de44bbfff0
Andres Borek <andres.b.dev@gmail.com>
2025-08-01 19:32:20 +0000

    Added missing .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,52 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es2021: true
   },
-  extends: 'standard',
-  overrides: [
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard'
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
+  plugins: [
+    '@typescript-eslint'
+  ],
   rules: {
-  }
+    // TypeScript specific rules
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    
+    // Standard JavaScript rules adjustments for TypeScript
+    'no-unused-vars': 'off', // Use TypeScript version instead
+    'no-undef': 'off', // TypeScript handles this
+    
+    // Code style rules
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'comma-dangle': ['error', 'never'],
+    'space-before-function-paren': ['error', 'always']
+  },
+  overrides: [
+    {
+      files: ['app/**/*.ts'],
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  ],
+  ignorePatterns: [
+    'dist/**',
+    'node_modules/**',
+    '*.js',
+    'lib/**/*.node'
+  ]
 }
